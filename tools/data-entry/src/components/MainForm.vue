4d7fb90c695a8126bb83b74731675976826a4cac
Jennifer Moore <jenniferplusplus@gmail.com>
2019-10-02 23:48:05 -0500

    tweaked the visuals and tweaked build scripts

--- tools/data-entry/src/components/MainForm.vue
+++ tools/data-entry/src/components/MainForm.vue
@@ -1,42 +1,47 @@
 <template>
-    <v-container>
-        <h1>Add a new list member</h1>
-        <v-form v-model="valid">
-            <v-container>
-                <v-row>
-                  <v-col><v-text-field v-model="full_name" label="Full Name" required/></v-col>
-                  <v-col>
-                    <v-text-field v-model="common_name" label="Common Name" required/>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col>
-                    <v-select v-model="pronouns" label="pronouns" :items="this.getPronounsList()"/>
-                  </v-col>
-                  <v-col>
-                    <v-text-field v-model="pronouns.subject" label="he/she/they" :value="pronouns.subject" required/>
-                  </v-col>
-                  <v-col>
-                    <v-text-field v-model="pronouns.object" label="him/her/them" required/>
-                  </v-col>
-                  <v-col>
-                    <v-text-field v-model="pronouns.possessive" label="his/hers/their" required/>
-                  </v-col>
-              </v-row>
-                <v-row>
-                    <v-textarea v-model="description" :label="`What makes ${pronouns.object ? pronouns.object : 'them'} notable?`" required></v-textarea>
-                </v-row>
-              <v-row>
-                <v-text-field v-model="birth_year" label="Birth Year"/>
-                <v-text-field v-model="birth_year" label="Death Year"/>
-              </v-row>
-            </v-container>
-        </v-form>
-    </v-container>
+  <v-container>
+    <h1>Add <em class="underline">{{common_name ? common_name : 'someone'}}</em> to the Tech List</h1>
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field v-model="full_name" label="Full Name" required/>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="common_name" label="Common Name" required/>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-select v-model="pronouns" label="pronouns" :items="this.getPronounsList()"/>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="pronouns.subject" label="he/she/they" :value="pronouns.subject"
+                          required/>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="pronouns.object" label="him/her/them" required/>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="pronouns.possessive" label="his/hers/their" required/>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-textarea v-model="description"
+                      :label="`What makes ${pronouns.object ? pronouns.object : 'them'} notable?`"
+                      required></v-textarea>
+        </v-row>
+        <v-row>
+          <v-text-field v-model="birth_year" label="Birth Year"/>
+          <v-text-field v-model="birth_year" label="Death Year"/>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
-  function factoryPronoun (s, o, p) {
+  function factoryPronoun(s, o, p) {
     return {subject: s, object: o, possessive: p}
   }
 
@@ -123,26 +128,26 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    h3 {
-        margin: 40px 0 0;
-    }
+  h3 {
+    margin: 40px 0 0;
+  }
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-    a {
-        color: #42b983;
-    }
+  a {
+    color: #42b983;
+  }
 
-    /*form.v-form div {*/
-    /*  padding-left: .5em;*/
-    /*  padding-right: .5em;*/
-    /*}*/
+  em.underline {
+    text-decoration: underline;
+    font-style: inherit;
+  }
 </style>
