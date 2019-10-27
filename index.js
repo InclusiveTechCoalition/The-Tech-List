803d30f629fd720f025288cdb90f5ee641de4e86
Chloe McKnight <cbt0029@gmail.com>
2019-10-27 11:43:23 -0500

    allow cors requests from frontend (#38)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express');
+require('dotenv').config();
 const {
     json
 } = require('body-parser');
@@ -11,6 +12,12 @@ const people = path.join(__dirname, 'People')
 const port = 3001;
 
 const app = express();
+
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 app.use(json());
 app.use('/photo', express.static(headshots))
